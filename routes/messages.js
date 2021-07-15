82ef2386dcab5b1f38edbe281377dd1b8ba091e3
ac-cool-breeze <alexcolter@gmail.com>
2021-07-15 16:54:12 -0500

    throwing shtuff at the wall

--- routes/messages.js
+++ routes/messages.js
@@ -24,8 +24,7 @@ router.get('/', function(req, res, next) {
       .then(data => res.status(200).json(data))
       .catch(err =>
         res.status(500).json({
-          message:
-            'problemo'
+          message: err
         })
       );
   });
