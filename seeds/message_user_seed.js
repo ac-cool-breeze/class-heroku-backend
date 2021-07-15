d821f1e4de6ea29d971d8c2417a2f2efa45e2e76
ac-cool-breeze <alexcolter@gmail.com>
2021-07-15 16:23:30 -0500

    corrected a knex seed file

--- seeds/message_user_seed.js
+++ seeds/message_user_seed.js
@@ -5,7 +5,7 @@
     return knex('messages_users').del()
       .then(function () {
         // Inserts seed entries
-        return knex('users').insert([
+        return knex('messages_users').insert([
           {id: 1, messages_id: 1, users_id: 1},
           {id: 2, messages_id: 2, users_id: 3},
           {id: 3, messages_id: 3, users_id: 2},
