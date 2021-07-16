e2dade9af155a4876a37fd78aee6911f65ee15ae
ac-cool-breeze <alexcolter@gmail.com>
2021-07-15 20:42:09 -0500

    changed connection string

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//const knex = require('knex')(require('./knexfile.js')['production']);
-const knex = require('knex')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
