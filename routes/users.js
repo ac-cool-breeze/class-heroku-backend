a1ff6df28de06927a7cab9af48b7405373580e0a
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 14:24:16 -0500

    moved handler to sign in

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.post('/login', function(req, res, next) {
   .select('name')
   .then(data => {
     if(data[0].name){
-      res.cookie('username', `${req.body.name}`)
+      res.cookie('username', `${req.body.name}`, { sameSite: 'none', secure: true})
       .send('Logged in')
       .status(200)
     } else {
