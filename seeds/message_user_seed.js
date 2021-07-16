bb81cdd17729b076b12cbfe1f8b4e56bd616cfca
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 10:40:35 -0500

    setup some routes

--- seeds/message_user_seed.js
+++ seeds/message_user_seed.js
@@ -6,9 +6,9 @@
       .then(function () {
         // Inserts seed entries
         return knex('messages_users').insert([
-          {id: 1, messages_id: 1, users_id: 1},
-          {id: 2, messages_id: 2, users_id: 3},
-          {id: 3, messages_id: 3, users_id: 2},
+          {messages_id: 1, users_id: 1},
+          {messages_id: 2, users_id: 3},
+          {messages_id: 3, users_id: 2},
         ]);
       });
   };
