cdfa3e98f2d923fc9971551985bcbf3ffd8c4d9b
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 11:25:56 -0500

    cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var messagesRouter = require('./routes/messages');
 
+var CORS = require('cors')
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var corsOptions = {
+  origin: 'https://class-heroku-frontend.herokuapp.com/'
+}
+
+app.use(cors(corsOptions))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
