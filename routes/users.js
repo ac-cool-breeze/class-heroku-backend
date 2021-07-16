89a1dc00b8bacd2f2e13bfa6ea658d66c1e39b92
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 09:42:21 -0500

    setup some routes

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,14 @@ const db = require('../db')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.select('*')
+  .from('users')
+  .then(data => res.status(200).json(data))
+  .catch(err =>
+    res.status(500).json({
+      message: err
+    })
+  );
 });
 
 router.post('/newuser', function(req, res, next) {
