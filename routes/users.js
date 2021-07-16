ef64504f5c9c8f7a88e1be4cdbdfbc8a23608057
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 10:12:14 -0500

    setup some routes

--- routes/users.js
+++ routes/users.js
@@ -36,7 +36,7 @@ router.post('/login', function(req, res, next) {
   .andWhere('password', req.body.password)
   .select('name')
   .then(data => {
-    console.log(data)
+    console.log(data.rows.length)
     if(data.rows.length > 0){
       res.cookie('username', `${req.body.name}`)
       .send('Logged in')
