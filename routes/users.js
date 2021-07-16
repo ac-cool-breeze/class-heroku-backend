239d213173d94f710879ad44a275328058c32c95
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 14:36:58 -0500

    asdfasdf

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.post('/login', function(req, res, next) {
   .select('name')
   .then(data => {
     if(data[0].name){
-      res.cookie('username', `${req.body.name}`, { sameSite: 'None', secure: true})
+      res.cookie('username', `${req.body.name}`, { sameSite: 'none', secure: true})
       .send('Logged in')
       .status(200)
     } else {
