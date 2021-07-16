280818c11fd13e069611e8cbfcba4e2ab757d09f
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 11:27:33 -0500

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var messagesRouter = require('./routes/messages');
 
-var CORS = require('cors')
+var cors = require('cors')
 
 var app = express();
 
